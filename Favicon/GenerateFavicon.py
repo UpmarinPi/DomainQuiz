8756983347c5d63dd6cd75e5809b467004c187c8
MDaiking <mdaiking117@gmail.com>
2025-05-15 10:17:22 +0900

    faviconの背景を透過

--- Favicon/GenerateFavicon.py
+++ Favicon/GenerateFavicon.py
@@ -9,16 +9,16 @@ fontname = "C:\\Windows\\Fonts\\arialbd.ttf"
 
 # canvas params
 canvas_size = (256, 256)
-background_rgb = (255, 255, 255)
-text_rgb = (0, 0, 0)
+background_rgba = (0, 255, 255, 0)
+text_rgba = (0, 0, 0, 0)
 
-img = Image.new('RGB', canvas_size, background_rgb)
+img = Image.new('RGBA', canvas_size, background_rgba)
 draw = ImageDraw.Draw(img)
 
 font = ImageFont.truetype(fontname, fontsize)
 text_width = draw.textlength(text, font=font)
 text_height = fontsize
 draw_topleft = ((canvas_size[0] / text_width) / 2, (canvas_size[1] - text_height) / 2)
-draw.text(draw_topleft, text, fill=text_rgb, font=font)
+draw.text(draw_topleft, text, fill=text_rgba, font=font)
 
 img.save(f"{savefolder}favicon.ico")
